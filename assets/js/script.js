649115b52c921e25b1a92c0480737ce4d8762bb4
coevpen <koinuevins@gmail.com>
2021-11-21 23:05:05 -0600

    add questions and answers object

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,126 @@ var questionEl = document.querySelector("#question-h1");
 // grabs the p element
 var openingEl = document.querySelector(".opening-p");
 
+// finds the section where we'll place the answers to be selected
+var answersEl = document.querySelector(".selections");
+
+//quiz object
+const quizQuestions = [
+  {
+      question: "Inside which HTML element do we put the JavaScript?",
+      answers: {
+          a: "<js>",
+          b: "<script>",
+          c: "<javascript>",
+          d: "<scripting>"
+
+      },
+      correctAnswer: "b"
+  },
+  {
+    question: "What is the correct syntax for referring to an external script called 'script.js'?",
+    answers: {
+        a: "<script href='script.js'>",
+        b: "<script name='script.js'>",
+        c: "<script class='script.js'>",
+        d: "<script src='script.js'>"
+
+    },
+    correctAnswer: "d"
+},
+{
+    question: "?",
+    answers: {
+        a: "<js>",
+        b: "<script>",
+        c: "<javascript>",
+        d: "<scripting>"
+
+    },
+    correctAnswer: "b"
+},
+{
+    question: "?",
+    answers: {
+        a: "<js>",
+        b: "<script>",
+        c: "<javascript>",
+        d: "<scripting>"
+
+    },
+    correctAnswer: "b"
+},
+{
+    question: "?",
+    answers: {
+        a: "<js>",
+        b: "<script>",
+        c: "<javascript>",
+        d: "<scripting>"
+
+    },
+    correctAnswer: "b"
+},
+{
+    question: "?",
+    answers: {
+        a: "<js>",
+        b: "<script>",
+        c: "<javascript>",
+        d: "<scripting>"
+
+    },
+    correctAnswer: "b"
+},
+{
+    question: "?",
+    answers: {
+        a: "<js>",
+        b: "<script>",
+        c: "<javascript>",
+        d: "<scripting>"
+
+    },
+    correctAnswer: "b"
+},
+{
+    question: "?",
+    answers: {
+        a: "<js>",
+        b: "<script>",
+        c: "<javascript>",
+        d: "<scripting>"
+
+    },
+    correctAnswer: "b"
+},
+{
+    question: "?",
+    answers: {
+        a: "<js>",
+        b: "<script>",
+        c: "<javascript>",
+        d: "<scripting>"
+
+    },
+    correctAnswer: "b"
+},
+{
+    question: "?",
+    answers: {
+        a: "<js>",
+        b: "<script>",
+        c: "<javascript>",
+        d: "<scripting>"
+
+    },
+    correctAnswer: "b"
+},
+
+];
+
+
+
 // counts down the timer
 function countdown(){
     var timeLeft = 75;
@@ -33,8 +153,10 @@ function displayQuestion(){
     
         switch(questionsNum){
             case 1:
-                questionEl.textContent = "What does HTML stand for?"
-                
+                questionEl.textContent = quizQuestions[0].question;
+
+                answersEl.innerHTML = 
+                "<button id='select-btn' class='option'>" + quizQuestions[0].answers.a + "</button> ";
                 break;
             case 2:
                 console.log("made it through switch: " + questionsNum);
@@ -74,13 +196,15 @@ function displayQuestion(){
 // when start button is clicked, start quiz and timer
 var startBtn = document.querySelector(".start-btn");
 startBtn.addEventListener("click", function(event){
+    document.querySelector(".start-btn").style.display = 'block';
+    this.style.display = 'none';
     countdown();
     openingEl.textContent = "";
     displayQuestion();
 });
 
 // change button color when clicked
-var changeBtnColor = document.querySelector("#select-btn");
+var changeBtnColor = document.querySelector(".option");
 changeBtnColor.addEventListener("click", () => changeBtnColor.style.backgroundColor = "#EE82EE");
 
 
@@ -88,3 +212,7 @@ changeBtnColor.addEventListener("click", () => changeBtnColor.style.backgroundCo
 buttons are dynamically created. 
 
 */
+
+function createAnswerButtons (){
+    //how???
+};
