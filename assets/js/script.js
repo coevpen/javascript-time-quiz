289773d4b74bbe23f5fa6ecbb37289b444b740b0
coevpen <koinuevins@gmail.com>
2021-11-26 21:17:36 -0600

    delete scores.js as it is duplicate, update script.js with all quiz qna

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ let hitMax = false;
 
 // selects the countdown html and starts with the timer set to 0 until quiz starts
 var timerEl = document.querySelector(".countdown");
-var timeLeft= 3;
+var timeLeft= 100;
 timerEl.textContent = "Timer: " + timeLeft;
 
 
@@ -58,66 +58,66 @@ const quizQuestions = [
         correctAnswer: "3"
     },
     {
-        question: "?",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
+        question: "What file extension does JavaScript have?",
+        answer1: ".html",
+        answer2: ".java",
+        answer3: ".js",
+        answer4: ".xml",
+        correctAnswer: "3",
 
     },
     {
-        question: "?",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
+        question: "Which of these functions displays a message and text entry?",
+        answer1: "prompt()",
+        answer2: "alert()",
+        answer3: "confirm()",
+        answer4: "msg()",
+        correctAnswer: "1",
 
     },
     {
-        question: "?",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
+        question: "What is the first index of an array?",
+        answer1: "0",
+        answer2: "1",
+        answer3: "-1",
+        answer4: "2",
+        correctAnswer: "1",
 
     },
     {
-        question: "?",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
+        question: "How would you select the class of 'container' inside of document.querySelector();?",
+        answer1: "#container",
+        answer2: ".container",
+        answer3: "--container",
+        answer4: ":container",
+        correctAnswer: "2",
 
     },
     {
-        question: "?",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
+        question: "How would you write an if statement that will execute code if two values were exactly the same?",
+        answer1: "if(i || k){}",
+        answer2: "if(i == k){}",
+        answer3: "if(i != k){}",
+        answer4: "if(i === k){}",
+        correctAnswer: "4",
 
     },
     {
-        question: "?",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
+        question: "Which is the correct way to write a for loop?",
+        answer1: "for(i = 0; i < 10){}",
+        answer2: "for(i){}",
+        answer3: "for(i = 0; i < 10; i++){}",
+        answer4: "for(i = 0; i++){}",
+        correctAnswer: "3",
 
     },
     {
-        question: "?",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
+        question: "How do you write a comment in JavaScript?",
+        answer1: "'this is a comment'",
+        answer2: "<!-- this is a comment -->",
+        answer3: "//this is a comment",
+        answer4: "## this is a comment",
+        correctAnswer: "3",
 
     },
 
